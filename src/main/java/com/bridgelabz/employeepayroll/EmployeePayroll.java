4d91e7381c2ffb07185bc4c330203229f6111ff3
MasterX01 <aakashsaxena14@gmail.com>
2021-03-31 19:38:37 +0530

    [Akash] FIRST COMMIT

---
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayroll.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeepayroll;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee PayRoll");
+    }
+}
