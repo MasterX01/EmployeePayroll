e7e16ce0da37b7c7ebceac66a02c3dc149941b1b
MasterX01 <aakashsaxena14@gmail.com>
2021-03-31 21:57:42 +0530

    [Akash] ADD. working UC-1 code

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayroll.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.employeepayroll;
-
-public class EmployeePayroll {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee PayRoll");
-    }
-}
